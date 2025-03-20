1600cf21c54ae9f3ba4ce719e1534d071cc02357
Cyberhero17 <ckirill042@gmail.com>
2025-03-20 22:40:04 +0300

    Game

---
+++ build/CMakeFiles/SpaceCat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceCat.dir/main.cpp.o"
+  "CMakeFiles/SpaceCat.dir/main.cpp.o.d"
+  "SpaceCat"
+  "SpaceCat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceCat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/some_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/some_test.dir/some_test.cpp.o"
+  "CMakeFiles/some_test.dir/some_test.cpp.o.d"
+  "some_test"
+  "some_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/some_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
